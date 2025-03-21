e1c086c689869e60a468109431594b597a91a5d1
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-03-21 17:47:48 +0330

    Update AppConfig.cs

--- dev/Common/AppConfig.cs
+++ dev/Common/AppConfig.cs
@@ -6,8 +6,8 @@ namespace RainbowFrame.Common;
 [GenerateAutoSaveOnChange]
 public partial class AppConfig : NotifiyingJsonSettings, IVersionable
 {
-    [EnforcedVersion("1.2.1.0")]
-    public Version Version { get; set; } = new Version(1, 1, 0, 0);
+    [EnforcedVersion("2.0.0.0")]
+    public Version Version { get; set; } = new Version(2, 0, 0, 0);
 
     private string fileName { get; set; } = Constants.AppConfigPath;
     private bool useTrayIcon { get; set; } = true;
